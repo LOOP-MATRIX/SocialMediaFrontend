c4a508c0d72ddc77a6026d1e087d957dd7177c37
learnandsail <fcs090sutaromkar@gmail.com>
2025-07-04 20:21:21 +0530

    changed the mode of env

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://pulselink-backend.onrender.com/api",
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "https://pulselink-backend.onrender.com/api",
   withCredentials: true,
 });
